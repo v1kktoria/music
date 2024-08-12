cdd0abaeb4b346d40d845fb9108789bb2163c5fd
v1kktoria <v.yanulevich4@gmail.com>
2024-08-13 00:32:04 +0300

    update webConfig

--- src/main/java/com/example/music/config/WebConfig.java
+++ src/main/java/com/example/music/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/C:/uploads/");
+                .addResourceLocations("file:/app/uploads/");
     }
 }
