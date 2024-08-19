2ecfe556d467c5f5989bd46b87cb17d1e07af806
v1kktoria <v.yanulevich4@gmail.com>
2024-08-19 13:27:46 +0300

    configured H2 in-memory database for testing

--- src/test/java/com/example/music/MusicApplicationTests.java
+++ src/test/java/com/example/music/MusicApplicationTests.java
@@ -3,11 +3,12 @@ package com.example.music;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MusicApplicationTests {
-
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+})
+public class MusicApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
